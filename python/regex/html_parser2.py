b36bb6115170246d4228e327e6af83eba44a3abf
ebelfer2020 <ebelfer@vectormax.com>
2021-04-02 14:23:43 -0600

    Adding regex and parsing

---
+++ python/regex/html_parser2.py
@@ -0,0 +1,16 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_comment(self, comment):
+        if '\n' in comment:
+            print('>>> Multi-line Comment')
+        else:
+            print('>>> Single-line Comment')
+
+        print(comment)
+
+    def handle_data(self, data):
+        if data == '\n': return
+        print('>>> Data')
+        print(data)
